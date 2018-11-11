32652b42fdf0035dc0e358d5b5944d143213b777
Axel Hodler <axel@hodler.co>
2018-11-11 22:29:48 +0100

    init with failing test

---
+++ src/test/kotlin/com/example/project/InitTests.kt
@@ -0,0 +1,13 @@
+package com.example.project
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class InitTests {
+
+    @Test
+    fun `fails`() {
+        assertEquals(true, false)
+    }
+
+}
