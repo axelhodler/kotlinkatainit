32740c84d28bdeec32ea33768b3ee531cc852b74
Axel Hodler <axel@hodler.co>
2018-11-12 20:38:51 +0100

    intellij kotlin autosetup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
 	kotlin("jvm") version "1.2.41"
 }
@@ -10,6 +12,7 @@ dependencies {
 	testCompile("org.junit.jupiter:junit-jupiter-api:5.3.1")
 	testCompile("org.junit.jupiter:junit-jupiter-params:5.3.1")
 	testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+	compile(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<Test> {
@@ -21,4 +24,12 @@ tasks.withType<Test> {
 
 tasks.withType<Wrapper> {
 	gradleVersion = "4.8"
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+	jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+	jvmTarget = "1.8"
 }
