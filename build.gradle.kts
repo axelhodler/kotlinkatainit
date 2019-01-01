755cf2afffb796ccc5bad840ac5b64bf1b3dff3f
Axel Hodler <axel@hodler.co>
2019-01-01 11:08:04 +0100

    get indentation in order

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,36 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.2.41"
+    kotlin("jvm") version "1.2.41"
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	testCompile("org.junit.jupiter:junit-jupiter-api:5.3.1")
-	testCompile("org.junit.jupiter:junit-jupiter-params:5.3.1")
-	testCompile("org.assertj:assertj-core:3.11.1")
-	testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.1")
-	compile(kotlin("stdlib-jdk8"))
+    testCompile("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testCompile("org.junit.jupiter:junit-jupiter-params:5.3.1")
+    testCompile("org.assertj:assertj-core:3.11.1")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+    compile(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-	testLogging {
-		events("passed", "skipped", "failed")
-	}
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
 
 tasks.withType<Wrapper> {
-	gradleVersion = "4.8"
+    gradleVersion = "4.8"
 }
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-	jvmTarget = "1.8"
+    jvmTarget = "1.8"
 }
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-	jvmTarget = "1.8"
+    jvmTarget = "1.8"
 }
